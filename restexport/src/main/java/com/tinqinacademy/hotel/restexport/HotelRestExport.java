3173dcde0181ad13618497015cfb62abcfd4b425
Daniel Stanchev <danyto0o@mail.bg>
2024-08-08 15:24:46 +0300

    t7 - fix HotelExport to return output pojo instead of Response Entity of Wildcard

--- restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
+++ restexport/src/main/java/com/tinqinacademy/hotel/restexport/HotelRestExport.java
@@ -1,15 +1,22 @@
 package com.tinqinacademy.hotel.restexport;
 
 import com.tinqinacademy.hotel.api.operations.addroom.AddRoomInput;
+import com.tinqinacademy.hotel.api.operations.addroom.AddRoomOutput;
 import com.tinqinacademy.hotel.api.operations.bookroom.BookRoomInput;
+import com.tinqinacademy.hotel.api.operations.bookroom.BookRoomOutput;
+import com.tinqinacademy.hotel.api.operations.deleteroom.DeleteRoomOutput;
+import com.tinqinacademy.hotel.api.operations.getavailablerooms.GetAvailableRoomsOutput;
+import com.tinqinacademy.hotel.api.operations.getroominfobyid.GetRoomInfoByIdOutput;
 import com.tinqinacademy.hotel.api.operations.registervisitor.RegisterVisitorInput;
+import com.tinqinacademy.hotel.api.operations.registervisitor.RegisterVisitorOutput;
+import com.tinqinacademy.hotel.api.operations.unbookroom.UnbookRoomOutput;
 import com.tinqinacademy.hotel.api.operations.updateroom.UpdateRoomInput;
+import com.tinqinacademy.hotel.api.operations.updateroom.UpdateRoomOutput;
 import com.tinqinacademy.hotel.api.operations.updateroompartially.UpdateRoomPartiallyInput;
+import com.tinqinacademy.hotel.api.operations.updateroompartially.UpdateRoomPartiallyOutput;
+import com.tinqinacademy.hotel.api.operations.visitorreportinfo.ReportVisitorsInfoOutput;
 import com.tinqinacademy.hotel.api.restapiroutes.RestApiRoutes;
-import feign.Param;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,29 +32,29 @@ import java.util.Optional;
 
 @FeignClient(name = "hotel")
 public interface HotelRestExport {
-    @GetMapping(RestApiRoutes.HOTEL_GET_AVAILABLE_ROOMS)
-     ResponseEntity<?> getAvailableRooms(@RequestParam(value = "startDate") LocalDate startDate,
-                                         @RequestParam(value = "endDate") LocalDate endDate,
-                                         @RequestParam(value = "bedCount") Integer bedCount,
-                                         @RequestParam(value = "bedSize") List<String> beds,
-                                         @RequestParam(value = "bathroomType") String bathroomType);
+
+     @GetMapping(RestApiRoutes.HOTEL_GET_AVAILABLE_ROOMS)
+     GetAvailableRoomsOutput getAvailableRooms(@RequestParam(value = "startDate") LocalDate startDate,
+                                               @RequestParam(value = "endDate") LocalDate endDate,
+                                               @RequestParam(value = "bedCount") Integer bedCount,
+                                               @RequestParam(value = "bedSize") List<String> beds,
+                                               @RequestParam(value = "bathroomType") String bathroomType);
 
     @GetMapping(RestApiRoutes.HOTEL_GET_AVAILABLE_ROOMS_BY_ID)
-//    @RequestLine(value = "GET /api/v1/hotel/{roomId}",decodeSlash = false)
-    ResponseEntity<?> getRoomInfoById(@PathVariable("roomId") String roomId);
+    GetRoomInfoByIdOutput getRoomInfoById(@PathVariable("roomId") String roomId);
 
     @PostMapping(RestApiRoutes.HOTEL_BOOK_ROOM)
-    ResponseEntity<?> bookRoom(@PathVariable("roomId") String roomId,
-                               @RequestBody BookRoomInput bookRoomInput);
+    BookRoomOutput bookRoom(@PathVariable("roomId") String roomId,
+                            @RequestBody BookRoomInput bookRoomInput);
 
     @DeleteMapping(RestApiRoutes.HOTEL_UNBOOK_ROOM)
-    ResponseEntity<?> unbook(@PathVariable("bookingId") String bookingId);
+    UnbookRoomOutput unbook(@PathVariable("bookingId") String bookingId);
 
     @PostMapping(RestApiRoutes.SYSTEM_REGISTER_VISITOR)
-    ResponseEntity<?> register(@RequestBody RegisterVisitorInput registerVisitorInput);
+    RegisterVisitorOutput register(@RequestBody RegisterVisitorInput registerVisitorInput);
 
     @GetMapping(RestApiRoutes.SYSTEM_REPORT_VISITOR_INFO)
-    ResponseEntity<?> report(
+    ReportVisitorsInfoOutput report(
         @RequestParam(required = false) Optional<LocalDate> startDate,
         @RequestParam(required = false) Optional<LocalDate> endDate,
         @RequestParam(required = false) Optional<String> firstName,
@@ -61,16 +68,16 @@ public interface HotelRestExport {
     );
 
     @PostMapping(RestApiRoutes.SYSTEM_CREATE_ROOM)
-    ResponseEntity<?> addRoom(@RequestBody AddRoomInput addRoomInput);
+    AddRoomOutput addRoom(@RequestBody AddRoomInput addRoomInput);
 
     @PutMapping(RestApiRoutes.SYSTEM_UPDATE_ROOM)
-    ResponseEntity<?> updateRoom(@RequestBody UpdateRoomInput updateRoomInput,
-                                 @PathVariable("roomId") String roomId);
+    UpdateRoomOutput updateRoom(@RequestBody UpdateRoomInput updateRoomInput,
+                                @PathVariable("roomId") String roomId);
 
     @PatchMapping(RestApiRoutes.SYSTEM_UPDATE_ROOM_PARTIALLY)
-    ResponseEntity<?> updateRoomPartially(@PathVariable("roomId") String roomId,
-                                          @RequestBody UpdateRoomPartiallyInput updateRoomPartiallyInput);
+    UpdateRoomPartiallyOutput updateRoomPartially(@PathVariable("roomId") String roomId,
+                                                  @RequestBody UpdateRoomPartiallyInput updateRoomPartiallyInput);
 
     @DeleteMapping(RestApiRoutes.SYSTEM_DELETE_ROOM)
-    public ResponseEntity<?> deleteRoom(@PathVariable("roomId") String roomId);
+    DeleteRoomOutput deleteRoom(@PathVariable("roomId") String roomId);
 }
